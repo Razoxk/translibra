13f2c3f6f770f95c33e1255a41405cad9bf3d997
Christopherkratos <c.r_urzua_@outlook.com>
2020-01-14 18:05:34 -0300

    footer

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+            <div class="widget-title">
+                            <img src="images/logot.png" width="180" height="62" aling="center" />
+                        </div> <!-- cierre de div logo -->
+ 
+						<div class="widget-title">
+                            
+                        </div>
+			
+							<p>Maria Luisa Santander 468 oficina 212 providencia – Santiago  contacto@translibra.cl  www.translibra.cl</p>
+
+					</div>
+				
+	
+				
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </footer><!-- end footer -->
