8f7b484d90a346fb7bb525418c9dbdf156e920b1
Pablo Orellana <porellana@tecnotec.cl>
2020-01-14 11:43:02 -0300

    add comentario hola mundo

--- navbar.php
+++ navbar.php
@@ -10,6 +10,8 @@
 								<li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
 								<li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
 								<li><a href="#"><i class="fa fa-rss-square" aria-hidden="true"></i></a></li>
+
+								<!-- HOLA MUNDO -->
 							<ul>
 						</div>
 					</div>
